cfb10a6da6715b505f01416ff9c853fb76a74848
Soham-igLigh8 <146626043+Soham-igLigh8@users.noreply.github.com>
2024-08-30 21:26:11 +0530

    Add files via upload
    a pretty useful speech recongnition who can give output by creating a  simple TxT file

---
+++ sp_recog.py
@@ -0,0 +1,32 @@
+import speech_recognition as sr
+import pyttsx3
+r = sr.Recognizer()
+def record_text():
+    while(1):
+        try:
+            with sr.Microphone() as source2:
+               r.adjust_for_ambient_noise(source2,duration=1)
+               audio2 = r.listen(source2)
+               MyText = r.recognize_google(audio2, language='en')
+        
+            return MyText
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+        except sr.UnknownValueError:
+            print("Unknown error occurredd")
+
+    return
+def output_text(text):
+   f = open("output.txt","a")
+   f.write(text)
+   f.write("\n")
+   f.close()
+   return
+while(1):
+   text = record_text()
+   output_text(text)
+   print("Wrote text")
+
+
+
+
